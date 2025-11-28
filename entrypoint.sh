f1622e64464b1122428ebc48e08a850cd7f51cf3
Christopher Johnstone <meson800@gmail.com>
2025-11-28 15:18:59 -0500

    Update entrypoint script cert check

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,19 +15,21 @@ openssl s_client -connect "$SERVER:$PORT" -showcerts </dev/null 2>/dev/null | op
 # Check certificate hashes to prevent man-in-the-middle attacks
 DOWNLOADED_HASH=$(sha256sum /tmp/server_cert.pem | cut -d ' ' -f 1)
 if [[ ! -v CERT_SHA256_HASH ]]; then 
-       printf "No certificate hash provided as environment variable CERT_SHA256_HASH\n"
-          printf "This is unsafe! If you want to trust this certificate, add this hash\n"
-             printf "into this container's environment:"
-                printf "%s\n" "$DOWNLOADED_HASH"
-                   exit 1
+    printf "No certificate hash provided as environment variable CERT_SHA256_HASH\n"
+    printf "This is unsafe! If you want to trust this server certificate, add this hash\n"
+    printf "into this container's environment:\n"
+    printf "%s\n" "$DOWNLOADED_HASH"
+    exit 1
 fi
 
-if ! echo "$CERT_SHA256_HASH  /tmp/server_cert.pem" | sha1sum --check --status; then
-       printf "Certificate hash failed! The downloaded certificate has hash:\n"
-          printf "%s\n" "$DOWNLOADED_HASH"
-             printf "but this does not match the provided one. Update the container environment\n"
-                printf "variable CERT_SHA256_HASH if you know that the certificate changed!"
-                   exit 1
+if ! printf "%s /tmp/server_cert.pem\n" "$CERT_SHA256_HASH" | sha1sum --check --status; then
+    printf "Certificate hash failed! The downloaded server certificate has hash:\n"
+    printf "%s\n" "$DOWNLOADED_HASH"
+    printf "but this does not match the provided one:"
+    printf "%s\n" "$CERT_SHA256_HASH"
+    printf "Update the container environment\n"
+    printf "variable CERT_SHA256_HASH if you know that the certificate changed!"
+    exit 1
 fi
 
 # Install the certificate
@@ -35,8 +37,8 @@ dsmcert -add -server "$SERVER" -file /tmp/server_cert.pem
 
 # If we aren't given arguments, run the default (dsmc scheduler)
 if [[ $# -eq 1 ]]; then
-        exec /bin/bash/dsmc schedule
-    else
-        # otherwise call whatever program the user requested (in the COMMAND arg)
-            exec "$@"
+    exec /bin/bash/dsmc schedule
+else
+    # otherwise call whatever program the user requested (in the COMMAND arg)
+    exec "$@"
 fi
