67bda37b6236b6930974dfd81768f554319c5a97
Christopher Johnstone <meson800@gmail.com>
2025-11-28 15:20:52 -0500

    Update entrypoint env variable and help text

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,21 +14,21 @@ openssl s_client -connect "$SERVER:$PORT" -showcerts </dev/null 2>/dev/null | op
 
 # Check certificate hashes to prevent man-in-the-middle attacks
 DOWNLOADED_HASH=$(sha256sum /tmp/server_cert.pem | cut -d ' ' -f 1)
-if [[ ! -v CERT_SHA256_HASH ]]; then 
-    printf "No certificate hash provided as environment variable CERT_SHA256_HASH\n"
+if [[ ! -v SERVER_CERT_SHA256_HASH ]]; then 
+    printf "No certificate hash provided as environment variable SERVER_CERT_SHA256_HASH\n"
     printf "This is unsafe! If you want to trust this server certificate, add this hash\n"
     printf "into this container's environment:\n"
     printf "%s\n" "$DOWNLOADED_HASH"
     exit 1
 fi
 
-if ! printf "%s /tmp/server_cert.pem\n" "$CERT_SHA256_HASH" | sha1sum --check --status; then
+if ! printf "%s /tmp/server_cert.pem\n" "$SERVER_CERT_SHA256_HASH" | sha1sum --check --status; then
     printf "Certificate hash failed! The downloaded server certificate has hash:\n"
     printf "%s\n" "$DOWNLOADED_HASH"
     printf "but this does not match the provided one:"
     printf "%s\n" "$CERT_SHA256_HASH"
-    printf "Update the container environment\n"
-    printf "variable CERT_SHA256_HASH if you know that the certificate changed!"
+    printf "Update the container environment variable SERVER_CERT_SHA256_HASH\n"
+    printf "if you know that the certificate changed!\n"
     exit 1
 fi
 
